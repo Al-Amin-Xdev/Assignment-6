3f93a9fb72cdbb723cb1f63f14831b4a8d77ed6c
Md. Al-Amin Mia <md.alamin.mia.webdev@gmail.com>
2025-09-07 22:34:57 +0600

    Fifth commit

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 
 let categoryApi = "https://openapi.programming-hero.com/api/categories";
 
+
 async function loadCategory (url) {
 
     try {
@@ -15,6 +16,8 @@ async function loadCategory (url) {
         let rawArray = responseConvertedToJson.categories;
 
         rawArray.forEach(arrayItem => {
+
+            let arrId = arrayItem.id;
             
             let allCategoryName = arrayItem.category_name;
 
@@ -24,10 +27,9 @@ async function loadCategory (url) {
 
             let newlyCreatedDiv = document.createElement('div');
 
-            newlyCreatedDiv.innerHTML = `<div class="button bg-green-800 m-1 p-2 rounded-md">
-                <button class="font-bold text-white text-xs text-left " >${allCategoryName}</button>
+            newlyCreatedDiv.innerHTML = `<div onclick="getCatIdOnly(${arrId})" class="button bg-green-800 m-1 p-2 rounded-md">
+                <button  class="font-bold text-white text-xs text-left " >${allCategoryName}</button>
             </div>`
-
             getCategoryDiv.appendChild(newlyCreatedDiv);
 
         });
@@ -67,8 +69,6 @@ async function loadAllPlant (url) {
             let plantCategory = arrayItem.category;
             let plantPrice = arrayItem.price;
 
-            console.log(plantNam);
-
 
             let getTreeDataDiv = document.getElementById('tree-data');
 
@@ -104,3 +104,75 @@ async function loadAllPlant (url) {
 }
 
 loadAllPlant (allPlantApi);
+
+
+
+// Category id and related plant 
+
+
+let catApi = "https://openapi.programming-hero.com/api/categories/";
+
+async function getCatIdOnly(id) {
+
+    try {
+
+        let res = await fetch(`https://openapi.programming-hero.com/api/category/${id}`);
+        let resConvertedToJson = await res.json();
+
+        // rawArray variable is the Array from server
+        
+        let rawArr = resConvertedToJson.plants;
+
+        console.log(rawArr);
+
+        let getTreeDataDiv = document.getElementById('tree-data');
+        getTreeDataDiv.innerHTML ="";
+
+
+        rawArr.forEach((items, index) => {
+            let plantFamily = items;
+            // console.log(items);
+
+             // Data to be shown
+            let familyImage = plantFamily.image;
+            let familyNam = plantFamily.name;
+            let familyDescription = plantFamily.description;
+            let familyCategory = plantFamily.category;
+            let familyPrice = plantFamily.price;
+
+            let newlyCreatedDivForTreeData = document.createElement('div');
+
+            newlyCreatedDivForTreeData.innerHTML = `<div class="card-1 p-1 border-2 w-full h-auto rounded-md md:w-full ">
+
+                <img class=" w-full h-[150px] bg-cover rounded-md" src="${familyImage}" alt="">
+
+                <h1 class="text-sm font-bold my-1" >${familyNam}</h1>
+
+                <p class="text-xs text-justify" >${familyDescription}</p>
+
+                <div class="tree-type flex flex-row  justify-between mt-2 p-1">
+                    <span class="text-xs font-bold bg-green-600 rounded-md px-2" >${familyCategory}</span>
+                    <span class="text-xs font-bold" >${familyPrice}</span>
+                </div>
+                
+                <div class="card-button flex justify-center mt-4">
+                    <button class="bg-green-700 rounded-md text-center text-sm font-bold text-white p-2" >Add to Cart</button>
+                </div>
+
+            </div>`;
+
+            getTreeDataDiv.appendChild(newlyCreatedDivForTreeData);
+            
+
+
+        });
+
+        
+
+
+    } catch (error) {
+        console.log("Error message: ", error);
+    }
+    
+}
+
