3666fb130db33c68bd7cf93a70ac524ca8634aa9
Md. Al-Amin Mia <md.alamin.mia.webdev@gmail.com>
2025-09-09 01:54:22 +0600

    7

--- script.js
+++ script.js
@@ -1,114 +1,271 @@
 
 
-// Loading the category dynamically 
-
-let categoryApi = "https://openapi.programming-hero.com/api/categories";
 
 
-async function loadCategory (url) {
 
-    try {
 
-        let response = await fetch(url);
-        let responseConvertedToJson = await response.json();
+// Middle Section-Dynamic Loading All Plants info by Id Category
+// ================================
 
-        // rawArray variable is the Array from server
+// Loading the category's Plants family dynamically
+//==============================================================================================================
 
-        let rawArray = responseConvertedToJson.categories;
+    // There are  2 broad steps to show the plant info according to id as, the API shows the category when added Id of category it shows the plants of this category 
+        
+        // 1. Creat a function which would take the id as  paramter, when the button(located) clicked inside section-1
+        // 2. onclcik the button will load the plants of that category API 
+        // 3. Creat a second function which whill be called from this function with the raw array as parameter
+        //-----------------------------------------------------------
 
-        rawArray.forEach(arrayItem => {
+    // Functionality or working steps of the  2nd functions
+        // 1.Receive the array from loadcategoryPlants function as a parameter
+        // 2.Get the container & empty the container as well , where the data will be shown
+        // 3.Loop through the array and get the data which will be shown
+            // 4.Creat the UI element ( Must be inside the loop)
+            // 5.Append chiled the the created element to parent ( Must be inside the loop)
+    //-----------------------------------------------------------------
 
-            let arrId = arrayItem.id;
-            
-            let allCategoryName = arrayItem.category_name;
 
-            // console.log(allCategoryName);
 
-            let getCategoryDiv = document.getElementById('cat');
+// Remove active class property with this function which will be call from the bellow function
+//=====================
 
-            let newlyCreatedDiv = document.createElement('div');
-
-            newlyCreatedDiv.innerHTML = `<div onclick="getCatIdOnly(${arrId})" class="button bg-green-600 m-1 p-2 rounded-md">
-                <button  class="font-bold text-white text-xs text-left " >${allCategoryName}</button>
-            </div>`
-            getCategoryDiv.appendChild(newlyCreatedDiv);
-
-        });
-
-    } catch (error) {
-        console.log("Error message:-", error);
-    }
+let removeclass = () => {
+    let getAllBtnClass = document.querySelectorAll('.all-btn');
     
+
+    // loop throught the all the button and remove active class
+
+    getAllBtnClass.forEach( btn => btn.classList.remove('active'));
 }
 
-loadCategory(categoryApi);
+//=======================
+    
+// This function is beng called from category_level buton from section-1
+//-------------------------------------------------------
+let loadCategoryPlants = (id) =>{
+
+    // 1. Getting the API data by fetch function-------------
+                // Add active status to selected button
+                    // Steps
+                        // 1. Give a common class to all button
+                        // 2. Creat a function that removes a pre made active class with css properties
+                        // 2. then add active status to the selected one
+
+    fetch(`https://openapi.programming-hero.com/api/category/${id}`)  //${id} will be received as parameter from section-1
+    .then( (response) => response.json())
+    .then((categoryPlantsArray) =>{
+
+        // Get the Selected category button with dynamic id
+        let categoryBtn = document.getElementById(`category-btn-${id}`);
+
+        // Remove the active class and later step add it so that it only shows on selected element
+
+        removeclass();  // This functions removes all the active class
+
+        // Add a (active class) to this button which will be shown only to selected one
+
+        categoryBtn.classList.add('active');
+        
+        displayPlantsByCategory(categoryPlantsArray.plants);
+    });
+
+};
+
+// 2nd Function
+
+let displayPlantsByCategory =  plantcategory =>{
+
+    // Get the container and empty it-------------------------------
+    let getPlantDiv = document.getElementById('tree-data');
+    getPlantDiv.innerHTML = "";
+
+    //Loop through the array and extract information-----------------
+
+    for(let item of plantcategory){
+        
+        // Information Extracted to be shown --------------------------
+        let plantimg = item.image;
+        let plantName = item.name;
+        let plantDescription = item.description;
+        let plantCategory = item.category;
+        let plantPrice = item.price;
+        let plantAddCartButtonId = item.id;
 
 
-// Load all the plants alltogether
+        // Creat a new container or element to show up the extreated data-----------------------
 
-let allPlantApi = "https://openapi.programming-hero.com/api/plants";
+        let newPlantDiv = document.createElement('div');
+        
+        newPlantDiv.innerHTML = ` <div class="card-1 p-1  border-2 w-full h-[330px] rounded-md md:w-full ">
 
-async function loadAllPlant (url) {
+                <img class=" w-full h-[150px] bg-cover rounded-md" src="${plantimg}" alt="">
 
-    try {
+                <h1 class="text-sm font-bold my-1" >${plantName}</h1>
 
-        let response = await fetch(url);
-        let responseConvertedToJson = await response.json();
-
-        // rawArray variable is the Array from server
-
-        let rawArrayOfTress = responseConvertedToJson.plants;
-
-        // console.log(rawArrayOfTress);
-
-        rawArrayOfTress.forEach(arrayItem => {
-            
-            // Data to be shown
-            let plantImage = arrayItem.image;
-            let plantNam = arrayItem.name;
-            let plantDescription = arrayItem.description;
-            let plantCategory = arrayItem.category;
-            let plantPrice = arrayItem.price;
-            let itemid = arrayItem.id;
-            
-
-
-            let getTreeDataDiv = document.getElementById('tree-data');
-
-            let newlyCreatedDivForTreeData = document.createElement('div');
-
-            newlyCreatedDivForTreeData.innerHTML = `<div class="card-1 p-1 border-2 w-full h-auto rounded-md md:w-full ">
-
-                <img class=" w-full h-[150px] bg-cover rounded-md" src="${plantImage}" alt="">
-
-                <h1 class="text-sm font-bold my-1" >${plantNam}</h1>
-
-                <p class="text-xs text-justify" >${plantDescription}</p>
+                <p class="text-xs text-justify hover:bg-green-200" >${plantDescription}</p>
 
                 <div class="tree-type flex flex-row  justify-between mt-2 p-1">
                     <span class="text-xs font-bold bg-green-600 rounded-md px-2" >${plantCategory}</span>
                     <span class="text-xs font-bold" >${plantPrice}</span>
                 </div>
                 
-                <div id="btndiv-${itemid}" onclick="getprice(${plantPrice}); addToCart('${plantNam}', ${plantPrice})" class="card-button flex justify-center mt-4">
-                    <button class="bg-green-700 rounded-md text-center text-sm font-bold text-white p-2" >Add to Cart</button>
+                <div id="cart-btn-${plantAddCartButtonId}" class="card-button flex justify-center mt-4">
+                    <button class="bg-green-700 rounded-md text-center text-sm font-bold text-white p-2" >
+                    Add to Cart</button>
                 </div>
 
             </div>`;
+
+        // Append the child to parent container
+        getPlantDiv.appendChild(newPlantDiv);
+
+
+    }
+
+
+};
+
+
+// Loading the category's Plants family dynamically with id is succesfull
+//==============================================================================================================
+
+
+
+
+
+
+
+
+// Left Section-1 -Dynamic Category
+// ================================
+
+// Loading the category dynamically
+//==============================================================================================================
+
+let loadCategory = () =>{
+    fetch("https://openapi.programming-hero.com/api/categories")
+    .then( (res) => res.json())
+    .then( (categoryData) => displayCategory(categoryData.categories));
+};
+
+loadCategory();
+
+let displayCategory = (categoryArray) => {
+
+    // To display the received data there are 4 steps
+        // 1.Get the container & empty the container as well , where the data will be shown
+        // 2.Loop through the array and get the data which will be shown
+            // 3.Creat the UI element ( Must be inside the loop)
+            // 4.Append chiled the the created element to parent ( Must be inside the loop)
+    //---------------------------------------------------------------------------------------------------------
+
     
-            getTreeDataDiv.appendChild(newlyCreatedDivForTreeData);
+    // 1.Get the container & empty the container as well
+    //---------------------------------------------------------------------------------------------------------       
+    let categoryContainer = document.getElementById('cat');
+    categoryContainer.innerHTML ="";
+
+    // 2.Loop through array and extract single item(which is object in this case) from the Category Array
+    //----------------------------------------------------------------------------------------------------------
+
+    for(let item of categoryArray){
+        
+        // CategoryName of each object from the array item-----------------------
+        let categoryName = item.category_name;
+
+        // CategoryId of each object from the array item--------------------------
+        let categoryId = item.id;
+
+        // Now creat the element--------------------------------------------------
+        let newElement = document.createElement('div');
+        newElement.innerHTML = `<div id="category-btn-${categoryId}" onclick="loadCategoryPlants(${categoryId})" class="button bg-green-400 m-1 p-2 rounded-md hover:bg-green-700 all-btn">
+                <button class="font-bold text-white " >${categoryName}</button>
+            </div> `;
+
+        // Append the chiled to prent container 
+        categoryContainer.appendChild(newElement);
+
+    };
+
+};
+
+displayCategory();
+
+// Category Name loading completed succesfully
+//=============================================================================================================
+
+
+
+
+
+
+
+// // Load all the plants alltogether
+
+// let allPlantApi = "https://openapi.programming-hero.com/api/plants";
+
+// async function loadAllPlant (url) {
+
+//     try {
+
+//         let response = await fetch(url);
+//         let responseConvertedToJson = await response.json();
+
+//         // rawArray variable is the Array from server
+
+//         let rawArrayOfTress = responseConvertedToJson.plants;
+
+//         // console.log(rawArrayOfTress);
+
+//         rawArrayOfTress.forEach(arrayItem => {
+            
+//             // Data to be shown
+//             let plantImage = arrayItem.image;
+//             let plantNam = arrayItem.name;
+//             let plantDescription = arrayItem.description;
+//             let plantCategory = arrayItem.category;
+//             let plantPrice = arrayItem.price;
+//             let itemid = arrayItem.id;
+            
+
+
+//             let getTreeDataDiv = document.getElementById('tree-data');
+
+//             let newlyCreatedDivForTreeData = document.createElement('div');
+
+//             newlyCreatedDivForTreeData.innerHTML = `<div class="card-1 p-1 border-2 w-full h-auto rounded-md md:w-full ">
+
+//                 <img class=" w-full h-[150px] bg-cover rounded-md" src="${plantImage}" alt="">
+
+//                 <h1 class="text-sm font-bold my-1" >${plantNam}</h1>
+
+//                 <p class="text-xs text-justify" >${plantDescription}</p>
+
+//                 <div class="tree-type flex flex-row  justify-between mt-2 p-1">
+//                     <span class="text-xs font-bold bg-green-600 rounded-md px-2" >${plantCategory}</span>
+//                     <span class="text-xs font-bold" >${plantPrice}</span>
+//                 </div>
+                
+//                 <div id="btndiv-${itemid}" onclick="getprice(${plantPrice}); addToCart('${plantNam}', ${plantPrice})" class="card-button flex justify-center mt-4">
+//                     <button class="bg-green-700 rounded-md text-center text-sm font-bold text-white p-2" >Add to Cart</button>
+//                 </div>
+
+//             </div>`;
+    
+//             getTreeDataDiv.appendChild(newlyCreatedDivForTreeData);
         
 
            
-        });
+//         });
 
-    } catch (error) {
-        console.log("Error message:-", error);
-    }
+//     } catch (error) {
+//         console.log("Error message:-", error);
+//     }
     
-}
+// }
 
-loadAllPlant (allPlantApi);
+// loadAllPlant (allPlantApi);
 
 // get the price
 
@@ -251,71 +408,71 @@ function addToCart(name, price) {
 // Category id and related plant 
 
 
-let catApi = "https://openapi.programming-hero.com/api/categories/";
+// let catApi = "https://openapi.programming-hero.com/api/categories/";
 
-async function getCatIdOnly(id) {
+// async function getCatIdOnly(id) {
 
-    try {
+//     try {
 
-        let res = await fetch(`https://openapi.programming-hero.com/api/category/${id}`);
-        let resConvertedToJson = await res.json();
+//         let res = await fetch(`https://openapi.programming-hero.com/api/category/${id}`);
+//         let resConvertedToJson = await res.json();
 
-        // rawArray variable is the Array from server
+//         // rawArray variable is the Array from server
         
-        let rawArr = resConvertedToJson.plants;
+//         let rawArr = resConvertedToJson.plants;
 
-        console.log(rawArr);
+//         console.log(rawArr);
 
-        let getTreeDataDiv = document.getElementById('tree-data');
-        getTreeDataDiv.innerHTML ="";
+//         let getTreeDataDiv = document.getElementById('tree-data');
+//         getTreeDataDiv.innerHTML ="";
 
 
-        rawArr.forEach((items, index) => {
-            let plantFamily = items;
-            // console.log(items);
+//         rawArr.forEach((items, index) => {
+//             let plantFamily = items;
+//             // console.log(items);
 
-             // Data to be shown
-            let familyImage = plantFamily.image;
-            let familyNam = plantFamily.name;
-            let familyDescription = plantFamily.description;
-            let familyCategory = plantFamily.category;
-            let familyPrice = plantFamily.price;
+//              // Data to be shown
+//             let familyImage = plantFamily.image;
+//             let familyNam = plantFamily.name;
+//             let familyDescription = plantFamily.description;
+//             let familyCategory = plantFamily.category;
+//             let familyPrice = plantFamily.price;
 
-            let newlyCreatedDivForTreeData = document.createElement('div');
+//             let newlyCreatedDivForTreeData = document.createElement('div');
 
-            newlyCreatedDivForTreeData.innerHTML = `<div class="card-1 p-1 border-2 w-full h-auto rounded-md md:w-full ">
+//             newlyCreatedDivForTreeData.innerHTML = `<div class="card-1 p-1 border-2 w-full h-auto rounded-md md:w-full ">
 
-                <img class=" w-full h-[150px] bg-cover rounded-md" src="${familyImage}" alt="">
+//                 <img class=" w-full h-[150px] bg-cover rounded-md" src="${familyImage}" alt="">
 
-                <h1 class="text-sm font-bold my-1" >${familyNam}</h1>
+//                 <h1 class="text-sm font-bold my-1" >${familyNam}</h1>
 
-                <p class="text-xs text-justify" >${familyDescription}</p>
+//                 <p class="text-xs text-justify" >${familyDescription}</p>
 
-                <div class="tree-type flex flex-row  justify-between mt-2 p-1">
-                    <span class="text-xs font-bold bg-green-600 rounded-md px-2" >${familyCategory}</span>
-                    <span class="text-xs font-bold" >${familyPrice}</span>
-                </div>
+//                 <div class="tree-type flex flex-row  justify-between mt-2 p-1">
+//                     <span class="text-xs font-bold bg-green-600 rounded-md px-2" >${familyCategory}</span>
+//                     <span class="text-xs font-bold" >${familyPrice}</span>
+//                 </div>
                 
-                <div class="card-button flex justify-center mt-4">
-                    <button class="bg-green-700 rounded-md text-center text-sm font-bold text-white p-2" >Add to Cart</button>
-                </div>
+//                 <div class="card-button flex justify-center mt-4">
+//                     <button class="bg-green-700 rounded-md text-center text-sm font-bold text-white p-2" >Add to Cart</button>
+//                 </div>
 
-            </div>`;
+//             </div>`;
 
-            getTreeDataDiv.appendChild(newlyCreatedDivForTreeData);
+//             getTreeDataDiv.appendChild(newlyCreatedDivForTreeData);
             
 
 
-        });
+//         });
 
         
 
 
-    } catch (error) {
-        console.log("Error message: ", error);
-    }
+//     } catch (error) {
+//         console.log("Error message: ", error);
+//     }
     
-};
+// };
 
 
 
